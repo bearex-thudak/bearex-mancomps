464bf42f3ee84578c2fc1bbd41c45f579a5d3bc1
Thomas Hudak <thudak734@gmail.com>
2022-07-07 08:06:43 -0400

    added bearex logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import "./Navbar.css";
 import { Link, Outlet } from "react-router-dom";
+import bxlogo from "../assets/logo-tagline.webp";
 
 const Navbar = () => {
   return (
     <div className="Navbar">
       <nav>
+        <img src={bxlogo} alt="bxlogo" />
         <Link to="/charts">Charts</Link> |{" "}
         <Link to="/dashboard">Dashboard</Link> |{" "}
         <Link to="/reports">Reports</Link> | <Link to="/Charts">Tables</Link> |{" "}
